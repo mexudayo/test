7af3ee9b2bb2e1bee3488efd3c4a61a955d6d200
ぺるるん <93661637+perurundayo@users.noreply.github.com>
2022-11-06 20:49:12 +0900

    ｵｯﾍﾟﾝﾀﾞﾊﾟｰｲwwwwwwww
    ｵｯﾍﾟﾝﾀﾞﾊﾟｰｲwwwwwwwwしてください。

--- Open The File.py
+++ Open The File.py
@@ -7,3 +7,5 @@ print("click on the open the file!!")
 print("HAHAHAHAHAHAHAHA")
 
 print("print文しか書けないバカですｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗｗ")
+
+print("printprint("printprint("printprint("printprint("printprint("printprint("printprint("printprint("printprint("printprint("printprint("printprint("printprint("print")")")")")")")")")")")")")")
